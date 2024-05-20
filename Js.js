08dd708b6e953653828df60b16ece6e4f4fa8baf
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-05-20 10:45:06 +0700

    first commit

---
+++ Js.js
@@ -0,0 +1,18 @@
+function kirimData() {
+    var nama = document.getElementById("nama").value
+    var nim = document.getElementById("nim").value
+    var peminatan = document.querySelector("input[name=peminatan]:checked").value
+    var alamat = document.getElementById("alamat").value
+    var angkatan = document.getElementById("angkatan").value
+    var tanggal = document.getElementById("tanggal").value
+
+
+    alert(
+        "Nama Mahasiswa : " + nama +
+        "\nNIM : " + nim +
+        "\nPeminatan : " + peminatan +
+        "\nAlamat : " + alamat +
+        "\nAngkatan : " + angkatan +
+        "\nTanggal : " + tanggal
+    )
+}
